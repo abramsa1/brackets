cce409cf48f0ef4f861d8b37e4449ca420ee9ae0
abramsa1 <abramsa1@rambler.ru>
2019-10-03 23:49:09 +0700

    feat: complete Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  
+    let bracket = '';
+    let buffer = [];
+    let char = '';
+    let repeat = '';
+
+    if (str.length % 2)
+        return false;
+    for (let i = 0; i < 2; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            bracket += bracketsConfig[j][i];
+            (bracketsConfig[j][i] === bracketsConfig[j][i + 1]) ? repeat += bracketsConfig[j][i]: 0;
+        }
+    }
+    for (let i = 0, k = 0; i < str.length; i++) {
+        if (bracket.slice(0, bracket.length / 2.).includes(str[i]) && !(repeat.includes(str[i]))) {
+            buffer[k] = str[i];
+            k++;
+        } else if (repeat.includes(str[i]) && char != str[i] && str[i] != buffer[buffer.length - 1]) {
+            buffer[k] = str[i];
+            k++;
+            char = str[i]
+        } else {
+            (repeat.includes(str[i])) ? char = '': 0;
+            if (bracket[bracket.indexOf(buffer[k - 1]) + bracket.length / 2] === str[i]) {
+                buffer.pop();
+                k--;
+            } else
+                return false;
+        }
+    }
+    if (buffer.length == 0)
+        return true;
+    return false;
 }
